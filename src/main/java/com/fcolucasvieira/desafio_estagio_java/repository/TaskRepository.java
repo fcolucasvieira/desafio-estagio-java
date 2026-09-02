717c7b458b11bbc9143bb5d29ec65ee361d86117
fcolucasvieira <lucas.vieira@alu.ufc.br>
2026-09-02 20:32:22 -0300

    feat(task): adds specific queries

--- src/main/java/com/fcolucasvieira/desafio_estagio_java/repository/TaskRepository.java
+++ src/main/java/com/fcolucasvieira/desafio_estagio_java/repository/TaskRepository.java
@@ -4,10 +4,20 @@ import com.fcolucasvieira.desafio_estagio_java.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
     Optional<Task> findById(UUID id);
+
+    List<Task> findByResponsibleContainingIgnoreCase(String responsible);
+
+    List<Task> findByDeliveryDate(LocalDate deliveryDate);
+
+    List<Task> findByCompletedFalse();
+
+    List<Task> findByCompletedFalseAndResponsibleContainingIgnoreCase(String responsible);
 }
