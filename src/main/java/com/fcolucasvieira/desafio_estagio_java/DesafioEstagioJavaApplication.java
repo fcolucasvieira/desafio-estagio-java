3f2210828483fc6291e22e415652e19551d764ef
fcolucasvieira <lucas.vieira@alu.ufc.br>
2026-09-02 16:26:10 -0300

    :tada: init project

---
+++ src/main/java/com/fcolucasvieira/desafio_estagio_java/DesafioEstagioJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fcolucasvieira.desafio_estagio_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEstagioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEstagioJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcolucasvieira/desafio_estagio_java/DesafioEstagioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcolucasvieira.desafio_estagio_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEstagioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
